d3157b465d8af78b60679b15ff1325329ca7c884
Christian Franzen <christian.franzen@rwth-aachen.de>
2026-07-24 23:10:47 +0200

    feat(appsync): Added support for configuration properties schemaWorkerThreads and schemaWorkerShutdownTimeoutSeconds
    (cherry picked from commit bdfe7521181e930422a3db36b2043672c7109de9)

--- testcontainers-floci/src/test/java/io/floci/testcontainers/config/services/AppSyncConfigTest.java
+++ testcontainers-floci/src/test/java/io/floci/testcontainers/config/services/AppSyncConfigTest.java
@@ -12,14 +12,20 @@ class AppSyncConfigTest {
     void shouldApplyDefaultAppSyncConfig() {
         AppSyncConfig config = AppSyncConfig.builder().build();
         assertThat(config.isEnabled()).isTrue();
+        assertThat(config.getSchemaWorkerThreads()).isEqualTo(4);
+        assertThat(config.getSchemaWorkerShutdownTimeoutSeconds()).isEqualTo(30);
     }
 
     @Test
     void shouldApplyCustomAppSyncConfig() {
         AppSyncConfig config = AppSyncConfig.builder()
                 .enabled(false)
+                .schemaWorkerThreads(8)
+                .schemaWorkerShutdownTimeoutSeconds(60)
                 .build();
         assertThat(config.isEnabled()).isFalse();
+        assertThat(config.getSchemaWorkerThreads()).isEqualTo(8);
+        assertThat(config.getSchemaWorkerShutdownTimeoutSeconds()).isEqualTo(60);
     }
 
     @Test
@@ -27,7 +33,25 @@ class AppSyncConfigTest {
         GenericContainer<?> container = genericContainer();
         AppSyncConfig.builder().build().applyEnvVarsToContainer(container);
 
-        assertThat(container.getEnvMap()).containsEntry("FLOCI_SERVICES_APPSYNC_ENABLED", "true");
+        assertThat(container.getEnvMap())
+                .containsEntry("FLOCI_SERVICES_APPSYNC_ENABLED", "true")
+                .containsEntry("FLOCI_SERVICES_APPSYNC_SCHEMA_WORKER_THREADS", "4")
+                .containsEntry("FLOCI_SERVICES_APPSYNC_SCHEMA_WORKER_SHUTDOWN_TIMEOUT_SECONDS", "30");
+    }
+
+    @Test
+    void shouldApplyCustomEnvVarsToContainer() {
+        GenericContainer<?> container = genericContainer();
+        AppSyncConfig.builder()
+                .schemaWorkerThreads(8)
+                .schemaWorkerShutdownTimeoutSeconds(60)
+                .build()
+                .applyEnvVarsToContainer(container);
+
+        assertThat(container.getEnvMap())
+                .containsEntry("FLOCI_SERVICES_APPSYNC_ENABLED", "true")
+                .containsEntry("FLOCI_SERVICES_APPSYNC_SCHEMA_WORKER_THREADS", "8")
+                .containsEntry("FLOCI_SERVICES_APPSYNC_SCHEMA_WORKER_SHUTDOWN_TIMEOUT_SECONDS", "60");
     }
 
     @Test
