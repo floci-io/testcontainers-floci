39703c683c8cb9e1fefcc6240324ce5fd11e6d69
Pedro Pietro <pedropietro@gmail.com>
2026-09-11 00:06:15 -0300

    Fix Error Status Code 400
    Error CloudControlServiceTest.shouldListResources:24 » UnsupportedAction ListResources is not supported for resource type AWS::SQS::Queue. change to AWS::S3::Bucket

--- testcontainers-floci/src/test/java/io/floci/testcontainers/services/CloudControlServiceTest.java
+++ testcontainers-floci/src/test/java/io/floci/testcontainers/services/CloudControlServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import software.amazon.awssdk.services.cloudcontrol.CloudControlClient;
 import software.amazon.awssdk.services.cloudcontrol.model.ResourceDescription;
+import software.amazon.awssdk.services.s3.S3Client;
 
 import java.util.List;
 
@@ -11,19 +12,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CloudControlServiceTest extends AbstractServiceTest {
 
+    private static final String BUCKET_NAME = "cloud-control-bucket";
+
     static CloudControlClient cloudControl;
 
     @BeforeAll
     static void setUp() {
         cloudControl = client(CloudControlClient.builder());
+
+        client(S3Client.builder().forcePathStyle(true)).createBucket(b -> b.bucket(BUCKET_NAME));
     }
 
     @Test
     void shouldListResources() {
         List<ResourceDescription> resources = cloudControl
-                .listResources(b -> b.typeName("AWS::SQS::Queue"))
+                .listResources(b -> b.typeName("AWS::S3::Bucket"))
                 .resourceDescriptions();
 
-        assertThat(resources).isNotNull();
+        assertThat(resources).anyMatch(r -> r.identifier().equals(BUCKET_NAME));
     }
 }
